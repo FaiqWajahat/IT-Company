ddb3ef534c80b851048aa380ee2567ff851d75e4
Faiq Wajahat <faiqwajahat20022@gmail.com>
2025-10-06 15:10:27 +0500

    sit map issues all done

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,17 @@
-// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: 'https://pixvion.com', // your domain
-  generateRobotsTxt: true,        // generates robots.txt automatically
-  sitemapSize: 5000,              // max URLs per sitemap file
-  changefreq: 'monthly',          // optional
-  priority: 0.7,                  // optional
+  siteUrl: 'https://pixvion.com',
+  generateRobotsTxt: true,
+  additionalPaths: async (config) => [
+    await config.transform(config, '/about'),
+    await config.transform(config, '/contact'),
+    await config.transform(config, '/services/website'),
+    await config.transform(config, '/services/app'),
+    await config.transform(config, '/services/graphics'),
+    await config.transform(config, '/projects'), 
+    await config.transform(config, '/technologies'),
+   await config.transform(config, '/confirmation'),
+  ],
 };
 
 export default config;
